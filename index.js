c583bc0f48c105aa7c219ce187a724ccd41de1f9
lyc <22719973+l-yc@users.noreply.github.com>
2020-11-05 20:55:40 +0800

    fix some embarrassing mistakes

--- index.js
+++ index.js
@@ -2,49 +2,32 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const port = 8888;
+const config = require('./config');
 
 const log = {
-  debug: require('debug')('deal:index:debug'),
-  error: require('debug')('deal:index:error')
+  debug: require('debug')('reversi:index:debug'),
+  error: require('debug')('reversi:index:error')
 };
 
 app.use(express.static(path.join(__dirname, '/public')));
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'pug');
-//
-//app.use('/slides', require('./controllers/slide-viewer.js')(express));
-//app.use('/browse', require('./controllers/slide-selector.js')(express));
-
-//app
-//  .listen(port, () => {
-//    log.debug('Server listening on port %d', port);
-//  })
-//  .on('error', function (err) {
-//    if(err.errno === 'EADDRINUSE') {
-//      log.error('Port %d is busy, terminating.', port);
-//    } else {
-//      log('Error encountered when launching server: %o', err);
-//    }
-//  });
 
 const http = require('http');
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(config.port);
 server.on('listening', () => {
-  log.debug('Server listening on port %d', port);
+  log.debug('Server listening on port %d', config.port);
 })
 server.on('error', err => {
   if(err.errno === 'EADDRINUSE') {
-    log.error('Port %d is busy, terminating.', port);
+    log.error('Port %d is busy, terminating.', config.port);
   } else {
     log('Error encountered when launching server: %o', err);
   }
 })
 
 const io = require('socket.io')(server);
-const crypto = require("crypto");
+const crypto = require('crypto');
 
 class Room {
   constructor(id) {
